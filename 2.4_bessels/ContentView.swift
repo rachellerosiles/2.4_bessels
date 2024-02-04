565e4df5377e63f443c69e64ff391a097431484e
Rachelle <rrosiles@hawk.iit.edu>
2024-02-03 18:27:41 -0600

    Initial Commit

---
+++ 2.4_bessels/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2.4_bessels
+//
+//  Created by PHYS 440 Rachelle on 2/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2.4_bessels/__4_besselsApp.swift
@@ -0,0 +1,17 @@
+//
+//  __4_besselsApp.swift
+//  2.4_bessels
+//
+//  Created by PHYS 440 Rachelle on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct __4_besselsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
