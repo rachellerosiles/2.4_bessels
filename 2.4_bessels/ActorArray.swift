14ca4c0e9a0c2640c99561067d1426928088608e
Rachelle <rrosiles@hawk.iit.edu>
2024-02-05 17:52:32 -0600

    commit

---
+++ 2.4_bessels/ActorArray.swift
@@ -0,0 +1,8 @@
+//
+//  ActorArray.swift
+//  2.4_bessels
+//
+//  Created by PHYS 440 Rachelle on 2/5/24.
+//
+
+import Foundation
---
+++ 2.4_bessels/Operators.swift
@@ -0,0 +1,8 @@
+//
+//  Operators.swift
+//  2.4_bessels
+//
+//  Created by PHYS 440 Rachelle on 2/5/24.
+//
+
+import Foundation
